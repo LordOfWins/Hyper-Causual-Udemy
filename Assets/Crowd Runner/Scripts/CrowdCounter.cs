4f8ee55d67c9acf88ba3c60a2f76321e2caa260f
sjyun011234@gmail.com <>
2025-12-18 15:17:05 +0900

    Initial Commit

---
+++ Assets/Crowd Runner/Scripts/CrowdCounter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class CrowdCounter : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TextMeshPro crowdCountText;
+    [SerializeField] private Transform crowdCountField;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crowdCountField.text = crowdCountText.childCount.ToString();
+    }
+}
